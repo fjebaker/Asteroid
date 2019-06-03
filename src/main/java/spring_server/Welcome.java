ae7e60e50eb3deae973f4b774ed669a6715c0afd
Moontemple <fergusbkr@gmail.com>
2019-06-03 17:02:10 +0100

    spring boot static server

---
+++ src/main/java/spring_server/Welcome.java
@@ -0,0 +1,24 @@
+package spring_server;
+
+public class Welcome {
+
+    private long id;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
