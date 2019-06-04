6ec97c74787bcf48e63d253e50294317d97e389f
Moontemple <fergusbkr@gmail.com>
2019-06-04 13:15:26 +0100

    initial work on user and music database - updated test framework

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import src.main.python.flaskserv.main as main
 
 if __name__ == "__main__":
-	main.app.run("127.0.0.1", "8080")
+	main.app.run("0.0.0.0", "8080")
--- src/main/python/flaskserv/Database.py
+++ src/main/python/flaskserv/Database.py
@@ -30,6 +30,7 @@ class DBInstance:
 	def create_table(self, table_name, **kwargs):
 		"""
 		Create a new table in the database.
+		If \'UNIQUE:col_name\' present in \\*\\*kwargs, adds a CONSTRAINT to the table called unique_col, making col_name unique.
 
 		:param table_name: name of table
 		:type table_name: str
@@ -37,10 +38,15 @@ class DBInstance:
 		:type \\*\\*kwargs: dict
 		"""
 		column_type = ""
+		constraint = ""
 		for c, t in kwargs.items():
+			if c == "UNIQUE":
+				constraint += ", CONSTRAINT unique_col UNIQUE ({})".format(t)
+				continue
 			column_type += c + " " + t + ", "
 		column_type = column_type[:-2]
-		self.cursor.execute('''CREATE TABLE {} ({});'''.format(table_name, column_type))
+
+		self.cursor.execute('''CREATE TABLE {} ({}{});'''.format(table_name, column_type, constraint))
 
 	def insert_entire_row(self, table_name, data):
 		"""
@@ -69,7 +75,7 @@ class DBInstance:
 		:param table_name: name of table
 		:type table_name: str
 		:param column: name of column to select from
-		:type column: str
+		:type column: str/list[str]
 		:returns: list of tuples with items from column [(x1, y1, ...), (x2, y2, ...), ...]
 		"""
 		if type(column_list) != list:
@@ -112,11 +118,119 @@ class DBInstance:
 			ok = False
 		return ok
 
-if __name__ == '__main__':
-	d = DBInstance(":memory:")
-	d.create_table("test_table", c1="text", c2="text", c3="real")
-	#print(d.get_column_info("test_table"))
-	d.insert_entire_row("test_table", ("test1", "test2", 9))
-	d.insert_entire_row("test_table", ("test1.1", "test2.1", 9.1))
-	print(d.select_columns("test_table", ["c1", "c3"]))
+class DBAccessory(type):
+	"""
+	Metaclass for the Database classes :class:`MusicDB` and :class:`UserDB`.
+	Will wrap all non-special functions with a decorator, providing a scoped instance of :class:`DBInstance`.
+	"""
+	def __new__(cls, name, bases, local):
+		for attr in local:
+			value = local[attr]
+			if "__" in attr:
+				# print("Skipping {}".format(attr))
+				continue
+			if callable(value):
+				local[attr] = DBAccessory.deco(value)
+		return type.__new__(cls, name, bases, local)
 
+	@classmethod
+	def deco(cls, func):
+		"""
+		Decorator which calls function with scoped :class:`DBInstance` in the class dict
+
+		:param func: function to decorate
+		:type func: function reference
+		"""
+		def wrapper(*args, **kwargs):
+			inst = args[0]						# class instance
+			with DBInstance(inst.db_handle) as inst.db_inst:
+				result = func(*args, **kwargs)
+			return result
+		return wrapper
+
+class MusicDB(metaclass=DBAccessory):
+	"""
+	Provides methods for interacting with the music database.
+	Encapsulates :class:`DBInstance`
+
+	Assumes database already has a table with format
+		
+		songs
+
+		name, artist, duration, meta_dat
+
+	:param db_handle: sqlite database handle
+	:type db_handle: str
+	"""
+	def __init__(self, db_handle):
+		self.db_handle = db_handle
+
+	def add_song(self, song_dict):
+		# {name:, artist:, duration:, meta_dat:}
+		self.db_inst.insert_entire_row("songs", 
+			[
+				song_dict["name"],
+				song_dict["artist"],
+				song_dict["duration"],
+				song_dict["meta_dat"]
+			])
+
+	def get_song_by_name(self, name):
+		pass
+
+	def get_song_by_artist(self, artist):
+		pass
+
+	def get_all_songs(self):
+		return self.db_inst.select_columns("songs",
+			["name", "artist", "duration", "meta_dat"])
+
+
+class UserDB(metaclass=DBAccessory):
+	"""
+	Provides methods for interacting with the static user database.
+	Encapsulates :class:`DBInstance`
+
+	Assumes database already has a table with format
+		
+		users
+
+		id, name, hash_pw, meta_dat
+
+	:param db_handle: sqlite database handle
+	:type db_handle: str
+	"""
+	def __init__(self, path):
+		self.db_handle = "HELLO WORLD"
+
+	def add_user(self, user_dict):
+		# {id, name, hash_pw, meta_dat}
+		pass
+
+	def get_user_by_id(self, id):
+		pass
+
+	def get_user_by_name(self, name):
+		pass
+
+	def get_all_users(self, name):
+		pass
+
+if __name__ == '__main__':
+	#with DBInstance("test.db") as db:
+	#	db.create_table("songs", name="text", artist="text", duration="real", meta_dat="text", UNIQUE='name, artist')
+#	with DBInstance("test.db") as db:
+		#db.create_table("songs", name="text", artist="text", duration="real", meta_dat="text")
+	#with DBInstance("test.db") as db:
+#		print(db.get_column_info("songs"))
+#	with DBInstance("test.db") as db:
+		#db.create_table("test_table", c1="text", c2="text", c3="real")
+#	with DBInstance("test.db") as db:
+#		print(db.get_column_info("test_tabl"))
+	mdb = MusicDB("test.db")
+#	mdb.add_song({"name":"You Too Must Die", "artist":"GOLD", "duration":333, "meta_dat":""})
+#	print(mdb.get_all_songs())
+#	mdb.add_song({"name":"You Too Must Die", "artist":"GOLD", "duration":333, "meta_dat":""})
+#	mdb.add_song({"name":"Plastic Boogie", "artist":"King Gizzard and the Lizard Wizard", "duration":181, "meta_dat":""})
+#	mdb.add_song({"name":"Fishing For Fishies", "artist":"King Gizzard and the Lizard Wizard", "duration":298, "meta_dat":""})
+	print(mdb.get_all_songs())
