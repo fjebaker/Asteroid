2df2f30a258e98ec3cc93a8462b30743348ba45e
Sam Hollow <zeac114@live.rhul.ac.uk>
2019-06-06 17:41:30 +0100

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name="Asteroid",
+    version="0.0.0",
+    packages=find_packages(),
+    author="Moontemple",
+    description="Music Server for parties and social gatherings.",
+    license="GNU GPLv3",
+    keywords="music",
+    url="https://github.com/Moontemple/Asteroid"
+)
