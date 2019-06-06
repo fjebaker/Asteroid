9ae0f9ccacc32f9c06954bedbb44b8790a823480
JrTheDragon01 <www.aereregion.webs.com@live.co.uk>
2019-06-06 23:07:34 +0100

    Formalised docstrings

--- src/main/web/static/script/json.js
+++ src/main/web/static/script/json.js
@@ -1,7 +1,10 @@
-//Sends a GET request for the json file at 'filename' and calls a callback depending on the result.
-//If the GET request is successful and returns a valid JSON file, successCallback is called with the JSON data.
-//If the GET request is successful but does not return a valid JSON file, successCallback is called with the status code as a string.
-//If the GET request is unsuccessful, failureCallback is called.
+/*
+ * Used to handle GET requests for JSON data distributed by the flask server
+ *
+ * @param {string} filename - the location to which the GET request should be sent
+ * @param {successCallback} successCallback - the callback to call upon successful loading of the JSON resource
+ * @param {failureCallback} failureCallback -  the callback to call upon timeout or error event of the GET request
+ */
 function getJson(filename,successCallback,failureCallback) {
     var rawFile = new XMLHttpRequest();
     rawFile.overrideMimeType("application/json");
@@ -21,5 +24,17 @@ function getJson(filename,successCallback,failureCallback) {
     rawFile.ontimeout = failure;
     rawFile.send();
 }
+
+/**
+ * The callback called upon the successful loading of a JSON resource accessed via GET request
+ * @callback successCallback
+ * @param {Object|string} data - If the GET request loads with status code 200 (OK), the JSON resource loaded. If the GET request loads with any other HTTP status code, returns the status code as a string (e.g "404")
+ */
+
+/**
+ * The callback called upon a timeout or error in accessing a JSON resource via GET request
+ * @callback failureCallback
+ */
+
 
 current_callback();
--- src/main/web/static/script/post.js
+++ src/main/web/static/script/post.js
@@ -2,6 +2,14 @@
 //On error or timeout, calls failureCallback with the request as arg.
 //On success, calls successCallback with the request as arg.
 //Concern: is it fine to use onload etc. for all?
+
+/*
+ * Used to handle POST requests to the flask server
+ *
+ * @param {FormData} data - the form data to be posted to the server
+ * @param {postCallback} successCallback - the callback to call upon success of the POST request
+ * @param {postCallback} failureCallback -  the callback to call upon timeout or error event of the POST request
+ */
 function postRequest(data,address,successCallback,failureCallback) {
     var request = new XMLHttpRequest();
     request.open('POST',address,true);
@@ -10,5 +18,11 @@ function postRequest(data,address,successCallback,failureCallback) {
     request.ontimeout = function(){failureCallback(request);};
     request.send(data);
 }
-    
+
+/**
+ * The callback called upon either the success or failure of a POST request
+ * @callback postCallback
+ * @param {XMLHttpRequest} request - the HTTP POST request in question
+ */
+
 current_callback();
