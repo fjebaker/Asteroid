f300ed364561a076d7d162b7291264fa434c0059
Moontemple <fergusbkr@gmail.com>
2019-06-09 03:01:36 +0100

    updated some player unit tests

--- src/test/python/test_player/test_PlayStream.py
+++ src/test/python/test_player/test_PlayStream.py
@@ -1,36 +1,42 @@
 import pytest
 import sys
-sys.path.append("src/main/python/player")
-sys.path.append("src/test/python/player")
+import unittest.mock as mock
 
-# mock alsaaudio
-module = type(sys)('alsaaudio')
-class MockStream:
-	setformat = ""
-	setchannels = ""
-	setrate = ""
-	setperiodsize = ""
-	def write(*args, **kwargs):
+# MOCK DEPENDENCIES
+sys.modules['alsaaudio'] = mock.MagicMock()
+
+
+import src.main.python.player.PlayStream as PlayStream
+import src.main.python.player.ConditionObject as ConditionObject
+
+def test_restrict_call():
+	@PlayStream.restrict_call
+	def test():
 		pass
-module.PCM = lambda a, b, c: MockStream()
-sys.modules['alsaaudio'] = module
-
-import PlayStream, ConditionObject
-import queue
-co = ConditionObject.ConditionObject()
-q = queue.Queue()
-ps = PlayStream.PlayStream(co, q)
-
-class TestPlayStream():
-
-	def test_restrict_call(self):
-		@PlayStream.restrict_call
-		def test():
-			pass
-
+	test()
+	with pytest.raises(Exception) as e:
 		test()
-		with pytest.raises(Exception) as e:
-			test()
+
+class TestPlayStream:
+
+	@mock.patch("wave.open")
+	@mock.patch("alsaaudio.PCM", return_value=mock.MagicMock())
+	def test_play(self, pcm, wopen):
+		queue = mock.MagicMock()
+		condObj = ConditionObject.ConditionObject()
+
+		ps = PlayStream.PlayStream(condObj, queue)
+		ps.loadsong(None)
+		wopen.assert_called_once()
+
+		ps.wf.readframes.side_effect = ["\x00\x00", False]
+
+		ps.run()
+
+		queue.put.assert_called_with(0)
+		assert condObj.done == True
+
+
 
 
 	
