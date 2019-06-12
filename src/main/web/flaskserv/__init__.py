e628799cf6f0e8a1471e0d3ebb4a48dbc964c134
Moontemple <fergusbkr@gmail.com>
2019-06-12 05:22:45 +0100

    tidied database wrapper - always returns dict, more reliable tuple use, better automation: updated unit tests for database, so atm fails Flask

--- src/main/web/flaskserv/__init__.py
+++ src/main/web/flaskserv/__init__.py
@@ -1,4 +1,4 @@
 from src.main.web.flaskserv.QueryHandlers import UserQuery, MusicQuery, BaseQuery
 from src.main.web.flaskserv.Database import DBAccessory, DBInstance, MusicDB, UserDB
 from src.main.web.flaskserv.FormHandlers import UserHandler
-from src.main.web.flaskserv.playqueue import Playlist, Vote
+from src.main.web.flaskserv.playqueue import Playlist, Vote, History
