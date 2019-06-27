e875db65eaccbdfbd48c986106f710f0a3da4f22
Sam Hollow <33451253+sambnh@users.noreply.github.com>
2019-06-27 20:21:41 +0100

    Update docs for FormHandlers.py

--- src/main/web/flaskserv/FormHandlers.py
+++ src/main/web/flaskserv/FormHandlers.py
@@ -1,3 +1,4 @@
+"""Contains all form handlers."""
 from src.main.web.flaskserv.Database import UserDB
 from flask import Response
 import os
@@ -5,25 +6,22 @@ import json
 
 
 class UserHandler():
-    """
-    Handles different requests related to user database.
-
-    :param request: :class:`flask:Request` instance containing the registration ``POST``.
-    :returns: :class:`flask:Response` with json string and status
-
-    ``status 404`` - error, with error message in json
-
-    ``status 201`` - success, with saved username in json
-    """
+    """Handle different requests related to user database."""
 
     def __init__(self, request):
+        """Initialise a UserHandler.
+
+        :param request: Instance containing the registration ``POST``.
+        :type request: flask:Request
+        """
         self.request = request
         self.form = request.form
 
     def add_user(self, name):
-        """
-        add user method, which adds the new user to the database.
-        Encapsulates :class:`src.main.python.flaskserv.Database.UserDB` to add new user with max(id) + 1 as id.
+        """Add a new user to the database.
+
+        Encapsulates: class: `src.main.python.flaskserv.Database.UserDB` to
+        add new user with max(id) + 1 as id.
         Starting id is 1.
 
         :param name: name of new user to add to database
@@ -40,11 +38,18 @@ class UserHandler():
         return new_id
 
     def __call__(self):
+        """TODO.
+
+        :return: Flask response with json string and status
+            ``status 404`` - error, with error message in json
+            ``status 201`` - success, with saved username in json
+        :rtype: flask:Response
+        """
         if "name" in self.form and self.request.__dict__["environ"]["REQUEST_METHOD"] == 'POST':
             new_id = self.add_user(self.form["name"])
             json_s = {"id": new_id}
             http_s = 201
         else:
-            json_s = {}				# todo, error message of what went wrong
+            json_s = {}				# TODO, error message of what went wrong
             http_s = 400
         return Response(json.dumps(json_s), status=http_s, mimetype='application/json')
