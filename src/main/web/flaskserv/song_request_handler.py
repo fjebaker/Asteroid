cb9637e92272f250598a15b7c42bfdc3977cb281
Sam Hollow <zeac114@live.rhul.ac.uk>
2019-07-16 13:23:40 +0100

    Convert songs downloaded song_request_handler.py to wav format

--- src/main/web/flaskserv/song_request_handler.py
+++ src/main/web/flaskserv/song_request_handler.py
@@ -1,9 +1,11 @@
 """Handle users requests for new songs."""
 import requests
 import flask
+import pydub
+import os
 
 
-def get(url):
+def get(url, music_path='src/main/music/', format='wav'):
     """Add the audio file at the url to the server.
 
     :param url: url of the song to be downloaded
@@ -13,13 +15,17 @@ def get(url):
     :raises TypeError: URL is not an audio file
     """
     song_name = url.rsplit('/', 1)[-1]
-    music_path = 'src/main/music/{song_name}'.format(song_name=song_name)
+    song_path = "".join([music_path, song_name])
     response = requests.get(url)
     mime_type = response.headers['Content-Type']
     if 'audio' not in mime_type:
         raise TypeError('{url} is not an audio file'.format(url=url))
-    with open(music_path, 'wb') as file:
+    with open(song_path, 'wb') as file:
         file.write(response.content)
+    new_song_path = ".".join([song_path.rsplit('.', 1)[0], format])
+    pydub.AudioSegment.from_file(song_path).export(new_song_path,
+                                                   format=format)
+    os.remove(song_path)
 
 
 def handle(request):
@@ -46,9 +52,9 @@ def handle(request):
     except FileNotFoundError as e:
         status = 400
     except requests.exceptions.MissingSchema as e:
-        print("{e}: '{url}' is an invalid URL".format(e=e,url=url))
+        print("{e}: '{url}' is an invalid URL".format(e=e, url=url))
         status = 400
     except requests.exceptions.ConnectionError as e:
-        print("{e}: Failed to connect to '{url}'".format(e=e,url=url))
+        print("{e}: Failed to connect to '{url}'".format(e=e, url=url))
         status = 400
     return flask.Response(status=status)
