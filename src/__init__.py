aa05bbbda6b56ec4350ba18d569fb01e467324db
dustpancake <fergusbkr@gmail.com>
2020-04-21 06:43:05 +0100

    dev 0.4: updated some __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,2 @@
 from src.main.databasebuilder.pyConfig import Config
 import src.main.databasebuilder.JSConfig as JSConfig
-import unittest.mock as mock
-import sys
-
-# MOCK ALSAAUDIO -- must be done before anything else is loaded into the pytest environment
-sys.modules['alsaaudio'] = mock.MagicMock()
