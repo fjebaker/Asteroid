c64eb1d53f20d9b681caf7c29eaf14e443911aaf
dustpancake <fergusbkr@gmail.com>
2020-04-21 06:44:54 +0100

    dev 0.4: removed commented code

--- config.py
+++ config.py
@@ -13,22 +13,3 @@ class Dev(Config):
 	#MONGO_URI = "mongodb://192.168.0.96:8934/asteroid"
 	MONGO_URI = "mongodb://192.168.0.12:27017/asteroid"
 	SERVE_FILES = True
-
-"""
-
-import pymongo
-from pymongo.errors import ServerSelectionTimeoutError
-if __name__ == '__main__':
-print(app.config['MONGO_URI'])
-try:
-	c=pymongo.MongoClient(
-		app.config['MONGO_URI'], 
-		serverSelectionTimeoutMS=app.config['MONGO_SERVER_SELECTION_TIMEOUT_MS']
-	)
-	# print(c.server_info())
-except ServerSelectionTimeoutError:
-	print("FAULTY DATABASE CONNECTION")
-	exit(1)
-
-app.run()
-"""
