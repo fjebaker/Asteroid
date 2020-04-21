b48832f96d821d9e4b179ed23d19a5fa3c0b9e8c
dustpancake <fergusbkr@gmail.com>
2020-04-21 08:27:19 +0100

    dev 0.42: updated pytest fixture environment

--- src/test/test_api.py
+++ src/test/test_api.py
@@ -1,9 +1,9 @@
 import pytest
 import json
 
-
 class TestUserEndpoint():
 
-	def test_byId(self, flaskclient):
+	def test_byId(self, flaskclient, user_model):
 		req = flaskclient.get('/db/users')
 		assert req.status_code == 200
+
